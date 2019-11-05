cb6068c58405bfd084de1ff8245f6a36fc2b0cfe
Ripe007 <42559908+Ripe007@users.noreply.github.com>
2019-11-05 16:14:49 +0800

    Update TokenAndMultiSignature.sol

--- TokenAndMultiSignature.sol
+++ TokenAndMultiSignature.sol
@@ -1,7 +1,7 @@
 /*
 需求1： Token管理
 在以太坊公链创建Token，并实现ERC20协议
-总量1依，不能增发、名称CIOS、单位CIOS、小数点6位
+总量1依，不能增发、名称CI、单位CI、小数点6位
 支持转账、被动转账、授权等基本功能
 支持Token销毁
 */
@@ -9,11 +9,11 @@
 pragma solidity >=0.4.22 <0.6.0;
 
 contract TokenERC20 {
-  address private owner; //合约创建者
 
   string public name; //Token名称
   string public symbol; //Token单位
   uint8 public decimals; //小数点
+
 
   uint256 public totalSuppy; //总发行量
 
@@ -31,9 +31,6 @@ contract TokenERC20 {
 
   //初始化参数
   constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _initiSuppy) public {
-    owner = msg.sender;
-    
-    
     name = _name;
     symbol = _symbol;
     decimals = _decimals;
@@ -41,28 +38,25 @@ contract TokenERC20 {
     balanceOf[msg.sender] = totalSuppy;
   }
 
-  modifier onlyOwner() {
-    require(owner == msg.sender);
-    _;
-  }
 
   //转账交易
-  function _transfer(address _from, address _to, uint256 _value) private {
+  function _transfer(address _from, address _to, uint256 _value) internal {
     require(balanceOf[_from] >= _value); //地址拥有的Token必须大于等于需要转账的value
     require(_to != address(0x0));  //交易地址不能为空
     require(balanceOf[_to] + _value > balanceOf[_to]); //防止溢出
 
-    uint256 previousBanlance = balanceOf[_from] + balanceOf[_to];
+    uint256 previousBanlance = balanceOf[_from] + balanceOf[_to]; //校验
     balanceOf[_from] -= _value;
     balanceOf[_to] += _value;
     emit Transfer(_from, _to, _value);
 
+    // 判断总额是否一致, 避免过程出错
     assert (balanceOf[_from] + balanceOf[_to] == previousBanlance);
   }
 
   //主动转账
-  function transfer(address _from, address _to, uint256 _value) public {
-    _transfer(_from, _to, _value);
+  function transfer(address _to, uint256 _value) public {
+    _transfer(msg.sender, _to, _value);
   }
 
   //被动转账
@@ -80,13 +74,25 @@ contract TokenERC20 {
   }
 
   //Token销毁
-  function destory(uint _value) private onlyOwner {
-    require(balanceOf[address(this)] >= _value);
+  function destory(uint _value) public {
+    require(balanceOf[msg.sender] >= _value);
+    balanceOf[msg.sender] -= _value;
+    totalSuppy -= _value;
+    emit Destory(msg.sender, _value);
+  }
+
+  //授权Token销毁
+  function destoryFrom(address _from, uint _value) public {
+    require(approvalBanlance[_from][msg.sender] >= _value);
+    require(balanceOf[_from] >= _value);
+    balanceOf[_from] -= _value;
+    approvalBanlance[_from][msg.sender] -= _value;
     totalSuppy -= _value;
     emit Destory(msg.sender, _value);
   }
 
 }
+
 
 
 /*
@@ -159,7 +165,7 @@ contract MultiSignature is TokenERC20{
     require(balanceOf[msg.sender] >= _amount);
 
     //转账到合约，等签名后在从合约转账给对应的地址
-    transfer(msg.sender, address(this), _amount);
+    _transfer(msg.sender, address(this), _amount);
 
     uint8 transcationid = transcationId++;
         
@@ -175,6 +181,20 @@ contract MultiSignature is TokenERC20{
     emit CreateTranscation(msg.sender, _to, _amount, transcationid);
   }
 
+  //查看队列
+  function getPendingTranscation() public view returns (uint) {
+    return pendingTranscation.length;
+  }
+  
+  //查看签名数
+  function getCounts(uint8 id) public view returns (uint256) {
+      return transcations[id].signatureCounts;
+  }
+  
+  function getBalance() public view returns (uint256) {
+      balanceOf[address(this)];
+  }
+ 
   //签名
   function multiSignature(uint8 id) public onlySigner {
     Transcation storage transcation = transcations[id];
@@ -188,7 +208,7 @@ contract MultiSignature is TokenERC20{
     if (transcation.signatureCounts >= MIN_SIGNATURE) {
       require(balanceOf[address(this)] >= transcation.amount);
 
-      transfer(address(this), transcation.to, transcation.amount);
+      _transfer(address(this), transcation.to, transcation.amount);
 
       //交易成功，移除
       delete pendingTranscation;  //从队列中移除，节省内存
@@ -198,3 +218,4 @@ contract MultiSignature is TokenERC20{
   }
 
 }
+
